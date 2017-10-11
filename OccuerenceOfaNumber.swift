405727c582f83f6a4a7b0c6773ca2fa1a3baaf69
Linkon <siddique.linkon@gmailcom>
2017-10-11 18:34:36 +0600

    comment added

--- OccuerenceOfaNumber.swift
+++ OccuerenceOfaNumber.swift
@@ -1,16 +1,16 @@
 class OccurenceNumber{
     
     static func getOccurence(of b:Int, in a:[Int])->Int{
-        guard a.contains(b) else {
+        guard a.contains(b) else { // if the element doesn't exist in array
             return 0
         }
         var getCount = 0
         var getPreviousCount = 0
         
         for i in 0..<a.count{
-            if a[i] != b{
+            if a[i] != b{ // if repeat sequence breaks then continue to the next index of the loop
                 if getCount > getPreviousCount{
-                    getPreviousCount = getCount
+                    getPreviousCount = getCount 
                 }
                 getCount = 0
                 continue
