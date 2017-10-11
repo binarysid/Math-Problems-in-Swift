3010207f8e3439dfc0835863707807ea8842196b
Linkon <siddique.linkon@gmailcom>
2017-10-11 18:22:43 +0600

    theory added as comment

--- Factorial.swift
+++ Factorial.swift
@@ -2,16 +2,16 @@ struct Factorial {
     
     func findFactorialOf(number:Int)->Int{
         
-        guard number > 0 else{
+        guard number > 0 else{ // avoid negative numbers
             return 0
         }
         var result:Int = 1
         
         for i in 0...number{
-            if number-i == 0 {
+            if number-i == 0 { // if last element
                 break;
             }
-            result = result*(number-i)
+            result = result*(number-i) // n(n-1)(n-2).....
         }
         return result
     }
