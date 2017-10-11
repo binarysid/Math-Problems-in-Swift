8f522a3bd00e31bf21bedddea33958d4042e3551
Linkon <siddique.linkon@gmailcom>
2017-10-11 17:55:53 +0600

    Simple Factorial Implementation

---
+++ Factorial.swift
@@ -0,0 +1,20 @@
+struct Factorial {
+    
+    func findFactorialOf(number:Int)->Int{
+        guard number > 0 else{
+            return 0
+        }
+        var result:Int = 1
+        for i in 0...number{
+            if number-i == 0 {
+                break;
+                
+            }
+            result = result*(number-i)
+            
+        }
+        return result
+    }
+}
+var fact = Factorial()
+print(fact.findFactorialOf(number:10))
